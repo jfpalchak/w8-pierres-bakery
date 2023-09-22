399980d7b915c5081f7d690d885cce251768c6bc
Joey Palchak <joey.palchak@gmail.com>
2023-09-22 15:58:32 -0700

    edit Order to update OrderTotal static variable

--- PierresBakery/Models/BakedGood.cs
+++ PierresBakery/Models/BakedGood.cs
@@ -4,7 +4,16 @@ namespace PierresBakery.Models
   {
     // auto-implemented properties
     public int Price { get; set; }
-    public int NthItemFree {get; set; }
+    public int NthItemFree { get; set; }
+
+    // create static field
+    // update field with virtual property
+    private static int _orderTotal;
+    public virtual int OrderTotal 
+    { 
+      get { return _orderTotal; } 
+      set { _orderTotal = value; }
+    }
 
     public int Order(int numberOfItems)
     {
@@ -12,14 +21,17 @@ namespace PierresBakery.Models
       {
         numberOfItems = 0;
       }
-      // calculate how many baked good items in the order is free
+      // calculate how many baked good items in the order are free:
       // number of items in the order / nth item that is free = (whole integer, rounded down)
       int freeItems = numberOfItems / NthItemFree;
       // calculate value of discount according to number of free items in user's order
       int discount = Price * freeItems;
       // calculate order's total cost and subtract discount
       int orderCost = (numberOfItems * Price) - discount;
-      return orderCost;
+      
+      // update static variable
+      OrderTotal += orderCost;
+      return OrderTotal;
     }
   }
 }
