1aa43ccd83f5571621ab75b294f79d61e02ab8ec
Joey Palchak <joey.palchak@gmail.com>
2023-09-22 12:35:02 -0700

    refactor UI to reflect changes to classes

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -7,12 +7,15 @@ namespace PierresBakery
   {
     static void Main()
     {
+      Bread newBread = new Bread();
+      Pastry newPastry = new Pastry();
+
       Console.WriteLine("==============================================");
       Console.WriteLine("Welcome to the order page for Pierre's Bakery!");
       Console.WriteLine("The bakery currently has the following items:");
       Console.WriteLine(" ---------------------------------------------");
-      Console.WriteLine("|     Bread: {0:C} |    Buy 2, get 1 free!    |", Bread.Price);
-      Console.WriteLine("|    Pastry: {0:C} |    Buy 3, get 1 free!    |", Pastry.Price);
+      Console.WriteLine("|     Bread: {0:C} |    Buy 2, get 1 free!    |", newBread.Price);
+      Console.WriteLine("|    Pastry: {0:C} |    Buy 3, get 1 free!    |", newPastry.Price);
       Console.WriteLine(" ---------------------------------------------");
       Console.WriteLine("How many loaves of bread would you like?");
       Console.WriteLine("Please enter a whole number:");
@@ -22,9 +25,9 @@ namespace PierresBakery
       string pastryOrderString = Console.ReadLine();
 
       int breadOrder = int.Parse(breadOrderString);
-      int breadCost = Bread.Order(breadOrder);
+      int breadCost = newBread.Order(breadOrder);
       int pastryOrder = int.Parse(pastryOrderString);
-      int pastryCost = Pastry.Order(pastryOrder);
+      int pastryCost = newPastry.Order(pastryOrder);
 
       Console.WriteLine("Here's your order:");
       Console.WriteLine(" ---------------------------------------------");
