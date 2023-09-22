938629793e4dde1172c2eefb4328f5fa54e4f7a5
Joey Palchak <joey.palchak@gmail.com>
2023-09-22 13:15:48 -0700

    remove virtual from BakedGood Order method

--- PierresBakery/Models/BakedGood.cs
+++ PierresBakery/Models/BakedGood.cs
@@ -2,10 +2,11 @@ namespace PierresBakery.Models
 {
   public abstract class BakedGood
   {
+    // auto-implemented properties
     public int Price { get; set; }
     public int NthItemFree {get; set; }
 
-    public virtual int Order(int numberOfItems)
+    public int Order(int numberOfItems)
     {
       // calculate how many baked good items in the order is free
       // number of items in the order / nth item that is free = (whole integer, rounded down)
