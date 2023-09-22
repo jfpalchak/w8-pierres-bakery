b57ea3d92982f575df4e37cbed9c0f90869858f5
Joey Palchak <joey.palchak@gmail.com>
2023-09-22 16:00:20 -0700

    add OrderTotal override, static field + ClearOrder

--- PierresBakery/Models/Bread.cs
+++ PierresBakery/Models/Bread.cs
@@ -4,10 +4,24 @@ namespace PierresBakery.Models
 {
   public class Bread : BakedGood
   {
+    // create new static field
+    // update new static field with inherited virtual property
+    private static int _orderTotalBread;
+    public override int OrderTotal
+    {
+      get { return _orderTotalBread; }
+      set { _orderTotalBread = value; }
+    }
+
     public Bread()
     {
       Price = 5;
       NthItemFree = 3;
+    }
+
+    public static void ClearOrder()
+    {
+      _orderTotalBread = 0;
     }
   }
 }
--- PierresBakery/Models/Pastry.cs
+++ PierresBakery/Models/Pastry.cs
@@ -4,10 +4,24 @@ namespace PierresBakery.Models
 {
   public class Pastry : BakedGood
   {
+    // create new static field
+    // update new static field with inherited virtual property
+    private static int _orderTotalPastry;
+    public override int OrderTotal
+    {
+      get { return _orderTotalPastry; }
+      set { _orderTotalPastry = value; }
+    }
+
     public Pastry()
     {
       Price = 2;
       NthItemFree = 4;
+    }
+
+    public static void ClearOrder()
+    {
+      _orderTotalPastry = 0;
     }
   }
 }
